00d2579f2aacfd6ac16ff5b208e32c0e4c8beea9
Aleksandr <alexlev83@list.ru>
2024-01-14 01:10:27 +0300

    addStudentGroup

---
+++ StudentGroup.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class StudentGroup {
+   
+    List<Student> studentList;
+
+    public StudentGroup(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+}
+
